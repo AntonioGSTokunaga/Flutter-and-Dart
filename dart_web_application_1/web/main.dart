38020d7977f1e0706d6e13430475bfd3d24475f5
Antonio Gabriel Satoshi Tokunaga <88676578+AntonioGSTokunaga@users.noreply.github.com>
2025-02-12 21:27:59 -0300

    Add files via upload

---
+++ dart_web_application_1/web/main.dart
@@ -0,0 +1,46 @@
+//import 'package:web/web.dart' as web;
+
+void main() {
+  PopulacaoCoelhos populacao = PopulacaoCoelhos(2, 0);
+  int mes = 1;
+  int duracao = 24;
+
+  while (mes <= duracao) {
+    populacao.progredirMes();
+
+    if (mes > 12) {
+      populacao.aplicarPredacao(0.75); // 75% de redução
+    }
+
+    print('Fim do Mês $mes: População de coelhos: ${populacao.total()}, Jovens: ${populacao.jovens}, Adultos: ${populacao.adultos}');
+
+    mes++;
+  }
+}
+
+class PopulacaoCoelhos {
+  int jovens;
+  int adultos;
+
+  PopulacaoCoelhos(this.jovens, this.adultos);
+
+  void progredirMes() {
+    int novosAdultos = jovens;
+    int novosJovens = adultos * 2;
+
+    adultos += novosAdultos;
+    jovens = novosJovens;
+  }
+
+  void aplicarPredacao(double taxa) {
+    int total = jovens + adultos;
+    int reducao = (total * taxa).toInt();
+    total -= reducao;
+
+    double proporcaoJovens = jovens / (jovens + adultos);
+    jovens = (total * proporcaoJovens).toInt();
+    adultos = total - jovens;
+  }
+
+  int total() => jovens + adultos;
+}
